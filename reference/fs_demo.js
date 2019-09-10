c330e9330eaa99a19d9a97077b5ec57eb4af0fb2
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-10 15:20:02 -0400

    learned how to write, append, and read from a file.

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -4,24 +4,31 @@ const path = require('path');
 // Create folder
 // fs.mkdir(path.join(__dirname, '/test'), {}, err => {
 //   if(err) throw err;
-//   console.log('Foler created...');
+//   console.log('Folder created...');
 // });
 
 
 // Create and write to file
-fs.writeFile(path.join(__dirname, '/test', 'hello.txt'),
-'Hello World!',
-err => {
-  if(err) throw err;
-  console.log('File written to...');
+// fs.writeFile(path.join(__dirname, '/test', 'hello.txt'),
+// 'Hello World!',
+// err => {
+//   if(err) throw err;
+//   console.log('File written to...');
+//
+//   // File append
+//   fs.appendFile(path.join(__dirname, '/test', 'hello.txt'),
+//   ' I log Node.js!',
+//   err => {
+//     if(err) throw err;
+//     console.log('File written to...');
+//     }
+//   );
+//   }
+// );
 
-  // File append
-  fs.appendFile(path.join(__dirname, '/test', 'hello.txt'),
-  ' I log Node.js!',
-  err => {
-    if(err) throw err;
-    console.log('File written to...');
-    }
-  );
-  }
-);
+
+// Read file
+fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+  if(err) throw err;
+  console.log(data);
+});
