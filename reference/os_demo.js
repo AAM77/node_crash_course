de4cc9df174c0c2df8780e256c609b7ae33923dd
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-10 15:29:38 -0400

    learns how to get core & memory info using .cpus(), .freemem() & .totalmem()

--- reference/os_demo.js
+++ reference/os_demo.js
@@ -6,3 +6,15 @@ console.log(os.platform());
 
 // CPU Arch
 console.log(os.arch());
+
+
+// CPU Core Info
+console.log(os.cpus());
+
+
+// Free memory
+console.log(os.freemem());
+
+
+// Total memory
+console.log(os.totalmem());
