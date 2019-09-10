266ba1f689991ef090e02e8e82b50b4b3a505d64
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-10 17:58:31 -0400

    Learned how to setup a server and how to create a script in package.json to let nodemon handle server refreshing for page updates.

--- index.js
+++ index.js
@@ -4,12 +4,30 @@
 //
 // person1.greeting();
 
-const Logger = require('./logger');
+// for user with logger.js
 
-const logger = new Logger();
+// const Logger = require('./logger');
+//
+// const logger = new Logger();
+//
+// logger.on('message', (data) => console.log('Called Listener:', data));
+//
+// logger.log('Hello World');
+// logger.log('Hi');
+// logger.log('Hello');
 
-logger.on('message', (data) => console.log('Called Listener:', data));
 
-logger.log('Hello World');
-logger.log('Hi');
-logger.log('Hello');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  if(req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html'});
+    res.end('<h1>Homepage</h1>')
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
