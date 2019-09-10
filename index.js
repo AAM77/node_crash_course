15c8a950175c859974b6a4c604e77de38bc5873d
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-10 18:08:03 -0400

    added code to handle loading the about page when url has /about

--- index.js
+++ index.js
@@ -26,8 +26,14 @@ const server = http.createServer((req, res) => {
     fs.readFile(path.join(__dirname, 'public', 'index.html'),
     (err, content) => {
       if (err) throw err;
-      res.writeHead(200, { 'Content-Type': 'text/html'});
-      res.end(content)
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content);
+    })
+  } else if (req.url === '/about') {
+    fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+      if (err) throw err;
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content);
     })
   }
 });
