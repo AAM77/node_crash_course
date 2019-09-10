4d8c0f39e027a7ab59878948e45e49be58097b02
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-10 15:21:36 -0400

    Learned how to rename a file.

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -28,7 +28,17 @@ const path = require('path');
 
 
 // Read file
-fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+// fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+//   if(err) throw err;
+//   console.log(data);
+// });
+
+
+// Rename file
+fs.rename(
+  path.join(__dirname, '/test', 'hello.txt'),
+  path.join(__dirname, '/test', 'helloWorld.txt'),
+  err => {
   if(err) throw err;
-  console.log(data);
+  console.log('File renamed....');
 });
